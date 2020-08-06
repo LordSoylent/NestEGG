e7216b6ca34e47c4273a7f1018e0e17f9d001f1f
Forhad Shamim <shamim.ice.ewu@gmail.com>
2020-08-06 11:25:58 +0600

    Update version.h

--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70918;
+static const int PROTOCOL_VERSION = 70919;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -20,11 +20,11 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70916;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70918;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70918;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70919;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 70916;
+static const int MIN_PEER_MNANNOUNCE = 70918;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
@@ -37,7 +37,7 @@ static const int BIP0031_VERSION = 60000;
 static const int MEMPOOL_GD_VERSION = 60002;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
-static const int NO_BLOOM_VERSION = 70005;
+static const int NO_BLOOM_VERSION = 70916;
 
 
 #endif // BITCOIN_VERSION_H
