b0cb450c3a6a6c49d2ef7b94c6e1eb59123479dc
sbrcoin <shamim.ice.ewu@gmail.com>
2021-01-27 15:06:50 +0000

    update protocal version

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70919;
+static const int PROTOCOL_VERSION = 70929;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -22,10 +22,10 @@ static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70918;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = PROTOCOL_VERSION;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70930;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 70913;
+static const int MIN_PEER_MNANNOUNCE = 70918;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
