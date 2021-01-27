45bdbeea4f00497e156759870ef87a71c2d0f9e2
Forhad Shamim <shamim.ice.ewu@gmail.com>
2021-01-28 04:34:51 +0600

    Update version.h

--- src/version.h
+++ src/version.h
@@ -22,10 +22,10 @@ static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70918;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70930;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = PROTOCOL_VERSION;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 70918;
+static const int MIN_PEER_MNANNOUNCE = 70913;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
